16dc53ac74a1174f080eadb1f729c027d1eb5d6a
Hemanth Katkoju <hemanth.katkoju@wisseninfotech.com>
2022-07-11 20:26:42 +0530

    Humanresource & person schemas

---
+++ HumanResources.sql
@@ -0,0 +1,160 @@
+CREATE SCHEMA HumanResources
+  CREATE TABLE Department(
+    DepartmentID SERIAL NOT NULL, -- smallint
+    Name "Name" NOT NULL,
+    GroupName "Name" NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Department_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE Employee(
+    BusinessEntityID INT NOT NULL,
+    NationalIDNumber varchar(15) NOT NULL,
+    LoginID varchar(256) NOT NULL,    
+    Org varchar NULL,-- hierarchyid, will become OrganizationNode
+    OrganizationLevel INT NULL, -- AS OrganizationNode.GetLevel(),
+    JobTitle varchar(50) NOT NULL,
+    BirthDate DATE NOT NULL,
+    MaritalStatus char(1) NOT NULL,
+    Gender char(1) NOT NULL,
+    HireDate DATE NOT NULL,
+    SalariedFlag "Flag" NOT NULL CONSTRAINT "DF_Employee_SalariedFlag" DEFAULT (true),
+    VacationHours smallint NOT NULL CONSTRAINT "DF_Employee_VacationHours" DEFAULT (0),
+    SickLeaveHours smallint NOT NULL CONSTRAINT "DF_Employee_SickLeaveHours" DEFAULT (0),
+    CurrentFlag "Flag" NOT NULL CONSTRAINT "DF_Employee_CurrentFlag" DEFAULT (true),
+    rowguid uuid NOT NULL CONSTRAINT "DF_Employee_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Employee_ModifiedDate" DEFAULT (NOW()),
+    CONSTRAINT "CK_Employee_BirthDate" CHECK (BirthDate BETWEEN '1930-01-01' AND NOW() - INTERVAL '18 years'),
+    CONSTRAINT "CK_Employee_MaritalStatus" CHECK (UPPER(MaritalStatus) IN ('M', 'S')), -- Married or Single
+    CONSTRAINT "CK_Employee_HireDate" CHECK (HireDate BETWEEN '1996-07-01' AND NOW() + INTERVAL '1 day'),
+    CONSTRAINT "CK_Employee_Gender" CHECK (UPPER(Gender) IN ('M', 'F')), -- Male or Female
+    CONSTRAINT "CK_Employee_VacationHours" CHECK (VacationHours BETWEEN -40 AND 240),
+    CONSTRAINT "CK_Employee_SickLeaveHours" CHECK (SickLeaveHours BETWEEN 0 AND 120)
+  )
+  CREATE TABLE EmployeeDepartmentHistory(
+    BusinessEntityID INT NOT NULL,
+    DepartmentID smallint NOT NULL,
+    ShiftID smallint NOT NULL, -- tinyint
+    StartDate DATE NOT NULL,
+    EndDate DATE NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_EmployeeDepartmentHistory_ModifiedDate" DEFAULT (NOW()),
+    CONSTRAINT "CK_EmployeeDepartmentHistory_EndDate" CHECK ((EndDate >= StartDate) OR (EndDate IS NULL))
+  )
+  CREATE TABLE EmployeePayHistory(
+    BusinessEntityID INT NOT NULL,
+    RateChangeDate TIMESTAMP NOT NULL,
+    Rate numeric NOT NULL, -- money
+    PayFrequency smallint NOT NULL,  -- tinyint
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_EmployeePayHistory_ModifiedDate" DEFAULT (NOW()),
+    CONSTRAINT "CK_EmployeePayHistory_PayFrequency" CHECK (PayFrequency IN (1, 2)), -- 1 = monthly salary, 2 = biweekly salary
+    CONSTRAINT "CK_EmployeePayHistory_Rate" CHECK (Rate BETWEEN 6.50 AND 200.00)
+  )
+  CREATE TABLE JobCandidate(
+    JobCandidateID SERIAL NOT NULL, -- int
+    BusinessEntityID INT NULL,
+    Resume XML NULL, -- XML(HRResumeSchemaCollection)
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_JobCandidate_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE Shift(
+    ShiftID SERIAL NOT NULL, -- tinyint
+    Name "Name" NOT NULL,
+    StartTime time NOT NULL,
+    EndTime time NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Shift_ModifiedDate" DEFAULT (NOW())
+  );
+
+COMMENT ON SCHEMA HumanResources IS 'Contains objects related to employees and departments.';
+
+SELECT 'Copying data into HumanResources.Department';
+\copy HumanResources.Department FROM './data/Department.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into HumanResources.Employee';
+\copy HumanResources.Employee FROM './data/Employee.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into HumanResources.EmployeeDepartmentHistory';
+\copy HumanResources.EmployeeDepartmentHistory FROM './data/EmployeeDepartmentHistory.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into HumanResources.EmployeePayHistory';
+\copy HumanResources.EmployeePayHistory FROM './data/EmployeePayHistory.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into HumanResources.JobCandidate';
+\copy HumanResources.JobCandidate FROM './data/JobCandidate.csv' DELIMITER E'\t' CSV ENCODING 'latin1';
+SELECT 'Copying data into HumanResources.Shift';
+\copy HumanResources.Shift FROM './data/Shift.csv' DELIMITER E'\t' CSV;
+
+-- Calculated column that needed to be there just for the CSV import
+ALTER TABLE HumanResources.Employee DROP COLUMN OrganizationLevel;
+
+-- Employee HierarchyID column
+ALTER TABLE HumanResources.Employee ADD organizationnode VARCHAR DEFAULT '/';
+-- Convert from all the hex to a stream of hierarchyid bits
+WITH RECURSIVE hier AS (
+  SELECT businessentityid, org, get_byte(decode(substring(org, 1, 2), 'hex'), 0)::bit(8)::varchar AS bits, 2 AS i
+    FROM HumanResources.Employee
+  UNION ALL
+  SELECT e.businessentityid, e.org, hier.bits || get_byte(decode(substring(e.org, i + 1, 2), 'hex'), 0)::bit(8)::varchar, i + 2 AS i
+    FROM HumanResources.Employee AS e INNER JOIN
+      hier ON e.businessentityid = hier.businessentityid AND i < LENGTH(e.org)
+)
+UPDATE HumanResources.Employee AS emp
+  SET org = COALESCE(trim(trailing '0' FROM hier.bits::TEXT), '')
+  FROM hier
+  WHERE emp.businessentityid = hier.businessentityid
+    AND (hier.org IS NULL OR i = LENGTH(hier.org));
+
+-- Convert bits to the real hieararchy paths
+CREATE OR REPLACE FUNCTION f_ConvertOrgNodes()
+  RETURNS void AS
+$func$
+DECLARE
+  got_none BOOLEAN;
+BEGIN
+  LOOP
+  got_none := true;
+  -- 01 = 0-3
+  UPDATE HumanResources.Employee
+   SET organizationnode = organizationnode || SUBSTRING(org, 3,2)::bit(2)::INTEGER::VARCHAR || CASE SUBSTRING(org, 5, 1) WHEN '0' THEN '.' ELSE '/' END,
+     org = SUBSTRING(org, 6, 9999)
+    WHERE org LIKE '01%';
+  IF FOUND THEN
+    got_none := false;
+  END IF;
+
+  -- 100 = 4-7
+  UPDATE HumanResources.Employee
+   SET organizationnode = organizationnode || (SUBSTRING(org, 4,2)::bit(2)::INTEGER + 4)::VARCHAR || CASE SUBSTRING(org, 6, 1) WHEN '0' THEN '.' ELSE '/' END,
+     org = SUBSTRING(org, 7, 9999)
+    WHERE org LIKE '100%';
+  IF FOUND THEN
+    got_none := false;
+  END IF;
+  
+  -- 101 = 8-15
+  UPDATE HumanResources.Employee
+   SET organizationnode = organizationnode || (SUBSTRING(org, 4,3)::bit(3)::INTEGER + 8)::VARCHAR || CASE SUBSTRING(org, 7, 1) WHEN '0' THEN '.' ELSE '/' END,
+     org = SUBSTRING(org, 8, 9999)
+    WHERE org LIKE '101%';
+  IF FOUND THEN
+    got_none := false;
+  END IF;
+
+  -- 110 = 16-79
+  UPDATE HumanResources.Employee
+   SET organizationnode = organizationnode || ((SUBSTRING(org, 4,2)||SUBSTRING(org, 7,1)||SUBSTRING(org, 9,3))::bit(6)::INTEGER + 16)::VARCHAR || CASE SUBSTRING(org, 12, 1) WHEN '0' THEN '.' ELSE '/' END,
+     org = SUBSTRING(org, 13, 9999)
+    WHERE org LIKE '110%';
+  IF FOUND THEN
+    got_none := false;
+  END IF;
+
+  -- 1110 = 80-1103
+  UPDATE HumanResources.Employee
+   SET organizationnode = organizationnode || ((SUBSTRING(org, 5,3)||SUBSTRING(org, 9,3)||SUBSTRING(org, 13,1)||SUBSTRING(org, 15,3))::bit(10)::INTEGER + 80)::VARCHAR || CASE SUBSTRING(org, 18, 1) WHEN '0' THEN '.' ELSE '/' END,
+     org = SUBSTRING(org, 19, 9999)
+    WHERE org LIKE '1110%';
+  IF FOUND THEN
+    got_none := false;
+  END IF;
+  EXIT WHEN got_none;
+  END LOOP;
+END
+$func$ LANGUAGE plpgsql;
+
+SELECT f_ConvertOrgNodes();
+-- Drop the original binary hierarchyid column
+ALTER TABLE HumanResources.Employee DROP COLUMN Org;
+DROP FUNCTION f_ConvertOrgNodes();
---
+++ Person.sql
@@ -0,0 +1,128 @@
+CREATE SCHEMA Person
+  CREATE TABLE BusinessEntity(
+    BusinessEntityID SERIAL, --  NOT FOR REPLICATION
+    rowguid uuid NOT NULL CONSTRAINT "DF_BusinessEntity_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_BusinessEntity_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE Person(
+    BusinessEntityID INT NOT NULL,
+    PersonType char(2) NOT NULL,
+    NameStyle "NameStyle" NOT NULL CONSTRAINT "DF_Person_NameStyle" DEFAULT (false),
+    Title varchar(8) NULL,
+    FirstName "Name" NOT NULL,
+    MiddleName "Name" NULL,
+    LastName "Name" NOT NULL,
+    Suffix varchar(10) NULL,
+    EmailPromotion INT NOT NULL CONSTRAINT "DF_Person_EmailPromotion" DEFAULT (0),
+    AdditionalContactInfo XML NULL, -- XML("AdditionalContactInfoSchemaCollection"),
+    Demographics XML NULL, -- XML("IndividualSurveySchemaCollection"),
+    rowguid uuid NOT NULL CONSTRAINT "DF_Person_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Person_ModifiedDate" DEFAULT (NOW()),
+    CONSTRAINT "CK_Person_EmailPromotion" CHECK (EmailPromotion BETWEEN 0 AND 2),
+    CONSTRAINT "CK_Person_PersonType" CHECK (PersonType IS NULL OR UPPER(PersonType) IN ('SC', 'VC', 'IN', 'EM', 'SP', 'GC'))
+  )
+  CREATE TABLE StateProvince(
+    StateProvinceID SERIAL,
+    StateProvinceCode char(3) NOT NULL,
+    CountryRegionCode varchar(3) NOT NULL,
+    IsOnlyStateProvinceFlag "Flag" NOT NULL CONSTRAINT "DF_StateProvince_IsOnlyStateProvinceFlag" DEFAULT (true),
+    Name "Name" NOT NULL,
+    TerritoryID INT NOT NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_StateProvince_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_StateProvince_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE Address(
+    AddressID SERIAL, --  NOT FOR REPLICATION
+    AddressLine1 varchar(60) NOT NULL,
+    AddressLine2 varchar(60) NULL,
+    City varchar(30) NOT NULL,
+    StateProvinceID INT NOT NULL,
+    PostalCode varchar(15) NOT NULL,
+    SpatialLocation varchar(44) NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_Address_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Address_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE AddressType(
+    AddressTypeID SERIAL,
+    Name "Name" NOT NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_AddressType_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_AddressType_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE BusinessEntityAddress(
+    BusinessEntityID INT NOT NULL,
+    AddressID INT NOT NULL,
+    AddressTypeID INT NOT NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_BusinessEntityAddress_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_BusinessEntityAddress_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE ContactType(
+    ContactTypeID SERIAL,
+    Name "Name" NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_ContactType_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE BusinessEntityContact(
+    BusinessEntityID INT NOT NULL,
+    PersonID INT NOT NULL,
+    ContactTypeID INT NOT NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_BusinessEntityContact_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_BusinessEntityContact_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE EmailAddress(
+    BusinessEntityID INT NOT NULL,
+    EmailAddressID SERIAL,
+    EmailAddress varchar(50) NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_EmailAddress_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_EmailAddress_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE Password(
+    BusinessEntityID INT NOT NULL,
+    PasswordHash VARCHAR(128) NOT NULL,
+    PasswordSalt VARCHAR(10) NOT NULL,
+    rowguid uuid NOT NULL CONSTRAINT "DF_Password_rowguid" DEFAULT (uuid_generate_v1()), -- ROWGUIDCOL
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_Password_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE PhoneNumberType(
+    PhoneNumberTypeID SERIAL,
+    Name "Name" NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_PhoneNumberType_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE PersonPhone(
+    BusinessEntityID INT NOT NULL,
+    PhoneNumber "Phone" NOT NULL,
+    PhoneNumberTypeID INT NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_PersonPhone_ModifiedDate" DEFAULT (NOW())
+  )
+  CREATE TABLE CountryRegion(
+    CountryRegionCode varchar(3) NOT NULL,
+    Name "Name" NOT NULL,
+    ModifiedDate TIMESTAMP NOT NULL CONSTRAINT "DF_CountryRegion_ModifiedDate" DEFAULT (NOW())
+  );
+
+COMMENT ON SCHEMA Person IS 'Contains objects related to names and addresses of customers, vendors, and employees';
+
+SELECT 'Copying data into Person.BusinessEntity';
+\copy Person.BusinessEntity FROM './data/BusinessEntity.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.Person';
+\copy Person.Person FROM './data/Person.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.StateProvince';
+\copy Person.StateProvince FROM './data/StateProvince.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.Address';
+\copy Person.Address FROM './data/Address.csv' DELIMITER E'\t' CSV ENCODING 'latin1';
+SELECT 'Copying data into Person.AddressType';
+\copy Person.AddressType FROM './data/AddressType.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.BusinessEntityAddress';
+\copy Person.BusinessEntityAddress FROM './data/BusinessEntityAddress.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.ContactType';
+\copy Person.ContactType FROM './data/ContactType.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.BusinessEntityContact';
+\copy Person.BusinessEntityContact FROM './data/BusinessEntityContact.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.EmailAddress';
+\copy Person.EmailAddress FROM './data/EmailAddress.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.Password';
+\copy Person.Password FROM './data/Password.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.PhoneNumberType';
+\copy Person.PhoneNumberType FROM './data/PhoneNumberType.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.PersonPhone';
+\copy Person.PersonPhone FROM './data/PersonPhone.csv' DELIMITER E'\t' CSV;
+SELECT 'Copying data into Person.CountryRegion';
+\copy Person.CountryRegion FROM './data/CountryRegion.csv' DELIMITER E'\t' CSV;
